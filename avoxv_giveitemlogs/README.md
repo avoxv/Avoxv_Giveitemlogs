86c7e88cabb06fa44459d9cd587eb0c6f7a3208b
avoxv <shortymobb5@gmail.com>
2026-02-25 20:38:25 -0500

    avoxv_giveitemlogs

---
+++ avoxv_giveitemlogs/README.md
@@ -0,0 +1,6 @@
+-- This script is Made by Avoxv for New Leaf
+
+How it looks spawning in chat..
+![preview](image.png) Hold Ctrl & Click (Image)
+
+Item Given Successfully!
---
+++ avoxv_giveitemlogs/fxmanifest.lua
@@ -0,0 +1,11 @@
+lua54 "yes"
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Avoxv Giveitem Logs'
+description 'GiveItem Notification Made for New Leaf'
+version '1.0.0'
+
+shared_script 'config.lua'
+server_script 'server.lua'
